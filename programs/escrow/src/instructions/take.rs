5dbd30fb5d7f563a20a4d74e1d7df854f9b264f4
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-11-02 16:35:17 +0100

    escrow:completed

--- programs/escrow/src/instructions/take.rs
+++ programs/escrow/src/instructions/take.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint,TokenAccount};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{Mint,TokenAccount, TokenInterface,TransferChecked, transfer_checked,CloseAccount, close_account}};
+use crate::state::Escrow;
+
 
 #[derive(Accounts)]
 
@@ -8,8 +10,9 @@ pub struct Take<'info>{
 pub taker: Signer<'info>,
 //In simple terms, the Mint account describes the rules and properties of a specific token on the Solana blockchain, which helps manage how tokens are created and how they function.
 
-
+#[account(mut)]
 pub maker: SystemAccount<'info>,
+//mint defines the properties of a token, like its supply, name, and the number of decimals it supports. Think of it as the “template”
 pub mint_a: InterfaceAccount<'info,Mint>,
 pub mint_b: InterfaceAccount<'info,Mint>,
 #[account(
@@ -31,5 +34,80 @@ pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
     associated_token::mint = mint_b,
     associated_token::authority = maker
 )]
-pub maker_ata_b: InterfaceAccount<'info,TokenAccount>
+pub maker_ata_b: InterfaceAccount<'info,TokenAccount>,
+#[account(
+    mut,
+    close = maker,
+    has_one = maker,
+    has_one = mint_a,
+    has_one = mint_b,
+    seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+    bump = escrow.bump
+)]
+pub escrow: Account<'info, Escrow>,
+
+#[account(
+    mut,
+    associated_token::mint = mint_a,
+    associated_token::authority = escrow,
+)]
+//TokenAccount is associated with a specific mint to know which type of token it is holding
+pub vault: InterfaceAccount<'info,TokenAccount>,
+pub associated_token_program: Program<'info , AssociatedToken>,
+pub token_program: Interface<'info, TokenInterface>,
+pub system_program: Program<'info, System>
+
+}
+
+impl<'info> Take<'info>{
+    pub fn deposit(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.taker_ata_b.to_account_info(),
+            to: self.maker_ata_b.to_account_info(),
+            authority: self.taker.to_account_info(),
+            mint: self.mint_b.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_ctx, self.escrow.receive, self.mint_b.decimals)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw_and_close_vault(&mut self) -> Result<()> {
+
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"escrow",
+            self.maker.key.as_ref(),
+            &self.escrow.seed.to_le_bytes()[..],
+            &[self.escrow.bump]
+        ]];
+        
+
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.vault.to_account_info(),
+            to: self.taker_ata_a.to_account_info(),
+            authority: self.escrow.to_account_info(),
+            mint: self.mint_a.to_account_info(),
+        };
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+        transfer_checked(cpi_context, self.vault.amount, self.mint_b.decimals)?;
+
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+ 
+ close_account(cpi_context)
+    }
 }
